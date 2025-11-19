df3ecd3409ec78f6e7c81a849c131b8d9433fccf
MIXQ <mrmixondqvest@gmail.com>
2025-11-19 20:07:03 +0300

    Added printCheck() and printList() funcs

--- Menu.java
+++ Menu.java
@@ -34,16 +34,64 @@ public class Menu extends IO {
                 return Pages.MENU;
             }
             trie.insertWord(command);
-            System.out.println("\tWord \"" + command + "\" wes added to the Trie");
+            System.out.println("\tWord \"" + command + "\" was added to the Trie");
+        }
+    }
+
+    public Pages printCheck() {
+        System.out.println("To check with prefix just type a word. Example: \"word\"");
+        System.out.println("To check for word type a word with a dot at the end. Example: \"word.\"");
+        while (true) { 
+            System.out.println("Type word or prefix you want to check or 0 to return:");
+            String command = input();
+            boolean isWord = (command.endsWith("."));
+            command = command.replace(".", "");
+            while (!isCorrectNumber(command, 0, 0) && !isCorrectString(command)) {
+                System.out.println("Wrong input!\nType word or prefix you want to check or 0 to return:");
+                command = input();
+                isWord = (command.endsWith("."));
+                command = command.replace(".", "");
+            }
+            if (isCorrectNumber(command, 0, 0)) {
+                return Pages.MENU;
+            }
+            if (isWord) {
+                if (trie.containsWord(command)) {
+                    System.out.println("\tWord \"" + command + "\" contains in the Trie");
+                } else {
+                    System.out.println("\tWord \"" + command + "\" does not contain in the Trie");
+                }
+            } else {
+                if (trie.startsWith(command)) {
+                    System.out.println("\tWord with prefix \"" + command + "\" contains in the Trie");
+                } else {
+                    System.out.println("\tWord with prefix \"" + command + "\" does not contain in the Trie");
+                }
+            }
         }
         
     }
 
-    public void printCheck() {
-
-    }
-
-    public void printList() {
-
+    public Pages printList() {
+        while (true) { 
+            System.out.println("Type prefix you want to find all words starting with or 0 to return:");
+            String command = input();
+            while (!isCorrectNumber(command, 0, 0) && !isCorrectString(command)) {
+                System.out.println("Wrong input!\nType prefix you want to find all words starting with or 0 to return:");
+                command = input();
+            }
+            if (isCorrectNumber(command, 0, 0)) {
+                return Pages.MENU;
+            }
+            String[] wordList = trie.getByPrefix(command).toArray(new String[0]);
+            if (wordList.length > 0) {
+                System.out.println("Found words starting with \"" + command + "\":");
+                for (String iter : wordList) {
+                    System.out.println(iter);
+                }
+            } else {
+                System.out.println("Nothing is found");
+            }  
+        }
     }
 }
